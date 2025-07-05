8e978acbd7a2b22ba9623833e1e719bad8336786
Abhishek Pratap Singh <apsingh394@gmail.com>
2025-07-05 22:51:28 +0530

    Serving dialer from nestjs

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { ConfigModule } from '@nestjs/config';
 import { AppService } from './app.service';
 import { DialerModule } from 'src/modules/dialer/dialer.module';
 import { CallingModule } from 'src/modules/calling/calling.module';
+import { join } from 'path';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
     }),
